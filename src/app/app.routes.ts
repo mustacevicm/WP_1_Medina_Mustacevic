0e930111597a941f5fe815e9005fae3b612e3562
Medina Mustačević <medinamedinadidi@gmail.com>
2025-11-28 14:50:27 +0100

    Aktivirane rute putem RouterLink-a, uređen app.html, uređen styles.css

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { About } from './about/about';
+import { Products } from './products/products';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: Home },
+    { path: 'about', component: About },
+    { path: 'products', component: Products },
+];
