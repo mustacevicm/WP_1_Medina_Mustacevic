8c2e23380955ffbcd4b10c43626f587561f2423f
Medina Mustačević <medinamedinadidi@gmail.com>
2025-11-28 15:30:33 +0100

    Uređena sekcija about

--- src/app/home/home.spec.ts
+++ src/app/home/home.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { Home } from './home';
 
 describe('Home', () => {
@@ -10,7 +9,7 @@ describe('Home', () => {
     await TestBed.configureTestingModule({
       imports: [Home]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(Home);
     component = fixture.componentInstance;
